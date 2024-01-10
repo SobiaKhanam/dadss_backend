f4849cd472c484d98b719011aa6d1c3166f1d882
SobiaKhanam <156082257+SobiaKhanam@users.noreply.github.com>
2024-01-10 11:03:39 +0500

    Delete intel directory

--- intel/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .intel_models import IntelReport, IntelReportDetails
-
-# Register your models here.
-
-admin.site.register(IntelReportDetails)
-admin.site.register(IntelReport)
